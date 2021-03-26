5d29de91a8dff03f5b386975da90804d14f5d421
Katie Huang <kofanhuang@gmail.com>
2021-03-25 19:08:09 -0700

    Love it column width

--- streamlit_app.py
+++ streamlit_app.py
@@ -532,7 +532,7 @@ def new_video_pick(f,fine):
         # stframe.image('data/' + best_frame)
         my_slot_result_image.image('data/' + best_frame)
         # my_slot_result_like
-        col1, col2,col3 = my_slot_result_like.beta_columns([1,0.25,0.2])
+        col1, col2,col3 = my_slot_result_like.beta_columns([1,0.26,0.2])
         # col1.markdown(":hearts:")
         col2.button('♥️...Love it!')
         col3.button('👿...Nope!')
@@ -809,7 +809,7 @@ if choice == "Customize picker":
 
             
             # my_slot_result_like
-            col1, col2,col3 = my_slot_caption_like.beta_columns([1,0.25,0.2])
+            col1, col2,col3 = my_slot_caption_like.beta_columns([1,0.28,0.2])
             # col1.markdown(":hearts:")
             col2.button('♥️...Love it!')
             col3.button('👿...Nope!')
